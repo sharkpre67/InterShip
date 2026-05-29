48648c46d411c90ffb9d27c5e06fb1fb0f797b45
Kha <minhkha696905@gmail.com>
2026-05-29 13:30:11 +0700

    Feat: Add Class_1

---
+++ Java_Intership/src/BT1/Class_1.java
@@ -0,0 +1,14 @@
+package BT1;
+
+public class Class_1 {
+	public static void main(String[] args) {
+	    // Prints "Hello World" followed by a new line
+	    System.out.println("Hello World"); 
+	    
+	    // Prints "Age: 25" on one line
+	    int age = 25;
+	    System.out.print("Age: ");
+	    System.out.println(age); 
+	  }
+	
+}
